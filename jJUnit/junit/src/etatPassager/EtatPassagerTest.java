c823ffa178038aa7b8f89d7be574df17c4276ba2
Denis_Guiraudet <guiraudet@live.fr>
2017-10-24 16:43:15 +0200

    Nouveau TP

---
+++ jJUnit/junit/src/etatPassager/EtatPassagerTest.java
@@ -0,0 +1,29 @@
+package etatPassager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EtatPassagerTest {
+
+	@Test
+	public void testEstExterieur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstAssis() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstDebout() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstInterieur() {
+		fail("Not yet implemented");
+	}
+
+}
