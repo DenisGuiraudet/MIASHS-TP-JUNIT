892a8c1f673254373bc65371ea05b8821d0e75cb
Kamideux <matthieu.orriere@gmail.com>
2017-12-06 10:01:05 +0100

    Partie 3 : Ajout classe

---
+++ jJUnit/Partie3/src/tec/Autobus.java
@@ -0,0 +1,11 @@
+package tec;
+
+public class Autobus implements Transport{
+
+	@Override
+	public void allerArretSuivant() throws UsagerInvalideException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
