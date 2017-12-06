b2a78e64f48320b8c4b3b91630112965683ccef5
Kamideux <matthieu.orriere@gmail.com>
2017-12-06 10:25:14 +0100

    Autobus compile

--- jJUnit/Partie3/src/tec/Autobus.java
+++ jJUnit/Partie3/src/tec/Autobus.java
@@ -1,9 +1,55 @@
 package tec;
 
-public class Autobus implements Transport{
+public class Autobus implements Transport, Bus{
 
+	
+	public Autobus(int assis, int debout) {
+		
+	}
 	@Override
 	public void allerArretSuivant() throws UsagerInvalideException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean aPlaceAssise() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean aPlaceDebout() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void demanderPlaceAssise(Passager p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void demanderPlaceDebout(Passager p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void demanderChangerEnDebout(Passager p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void demanderChangerEnAssis(Passager p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void demanderSortie(Passager p) {
 		// TODO Auto-generated method stub
 		
 	}
